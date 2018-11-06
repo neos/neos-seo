2db2164e280c2e9c8ca9403f224e932ceeb5db43
Sebastian Helzle <sebastian@helzle.it>
2018-11-06 19:31:41 +0100

    FEATURE: New sitemap implementation including images
    This change uses a new implementation for the sitemap
    to increase rendering speed and optionally allow rendering
    of images in the sitemap.
    
    Resolves: #41

---
+++ Classes/Fusion/XmlSitemapImplementation.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Neos\Seo\Fusion;
+
+/*
+ * This file is part of the Neos.Seo package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\ContentRepository\Exception\NodeException;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Persistence\Doctrine\PersistenceManager;
+use Neos\Media\Domain\Model\ImageInterface;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\ContentRepository\Domain\Model\NodeType;
+use Neos\ContentRepository\Domain\Service\NodeTypeManager;
+use Neos\Fusion\FusionObjects\TemplateImplementation;
+
+class XmlSitemapImplementation extends TemplateImplementation
+{
+    /**
+     * @Flow\Inject
+     * @var NodeTypeManager
+     */
+    protected $nodeTypeManager;
+
+    /**
+     * @Flow\Inject
+     * @var PersistenceManager
+     */
+    protected $persistenceManager;
+
+    /**
+     * @var array
+     */
+    protected $assetPropertiesByNodeType;
+
+    /**
+     * @return bool
+     */
+    public function getIncludeImageUrls(): bool
+    {
+        return $this->fusionValue('includeImageUrls');
+    }
+
+    /**
+     * @return bool
+     */
+    public function getRenderHiddenInIndex(): bool
+    {
+        return $this->fusionValue('renderHiddenInIndex');
+    }
+
+    /**
+     * @return NodeInterface
+     */
+    public function getStartingPoint(): NodeInterface
+    {
+        return $this->fusionValue('startingPoint');
+    }
+
+    /**
+     * @return void
+     */
+    public function initializeObject()
+    {
+        $relevantPropertyTypes = [
+            'array<Neos\Media\Domain\Model\Asset>' => true,
+            'Neos\Media\Domain\Model\Asset' => true,
+            'Neos\Media\Domain\Model\ImageInterface' => true
+        ];
+        foreach ($this->nodeTypeManager->getNodeTypes(false) as $nodeType) {
+            /** @var NodeType $nodeType */
+            foreach ($nodeType->getProperties() as $propertyName => $propertyConfiguration) {
+                if (isset($relevantPropertyTypes[$nodeType->getPropertyType($propertyName)])) {
+                    $this->assetPropertiesByNodeType[$nodeType->getName()][] = $propertyName;
+                }
+            }
+        }
+    }
+
+    /**
+     * @return array
+     * @throws NodeException
+     */
+    public function getItems(): array
+    {
+        $items = [];
+
+        $this->appendItems($items, $this->getStartingPoint());
+
+        return $items;
+    }
+
+    /**
+     * @param array & $items
+     * @param NodeInterface $node
+     * @return void
+     * @throws NodeException
+     */
+    protected function appendItems(array &$items, NodeInterface $node)
+    {
+        if ($this->isDocumentNodeToBeIndexed($node)) {
+            $item = [
+                'node' => $node,
+                'lastModificationDateTime' => $node->getNodeData()->getLastModificationDateTime(),
+                'priority' => $node->getProperty('xmlSitemapPriority') ?: '0.5',
+                'images' => [],
+            ];
+            if ($node->getProperty('xmlSitemapChangeFrequency')) {
+                $item['changeFrequency'] = $node->getProperty('xmlSitemapChangeFrequency');
+            }
+            if ($this->getIncludeImageUrls()) {
+                $this->resolveImages($node, $item);
+            }
+            $items[] = $item;
+        }
+        if (!$node->hasChildNodes()) {
+            return;
+        }
+        foreach ($node->getChildNodes('Neos.Neos:Document') as $childDocumentNode) {
+            $this->appendItems($items, $childDocumentNode);
+        }
+    }
+
+    /**
+     * @param NodeInterface $node
+     * @param array & $item
+     * @return void
+     * @throws NodeException
+     */
+    protected function resolveImages(NodeInterface $node, array & $item)
+    {
+        if (isset($this->assetPropertiesByNodeType[$node->getNodeType()->getName()]) && !empty($this->assetPropertiesByNodeType[$node->getNodeType()->getName()])) {
+
+            foreach ($this->assetPropertiesByNodeType[$node->getNodeType()->getName()] as $propertyName) {
+                if (is_array($node->getProperty($propertyName)) && !empty($node->getProperty($propertyName))) {
+                    foreach ($node->getProperty($propertyName) as $asset) {
+                        if ($asset instanceof ImageInterface) {
+                            $item['images'][$this->persistenceManager->getIdentifierByObject($asset)] = $asset;
+                        }
+                    }
+                } elseif ($node->getProperty($propertyName) instanceof ImageInterface) {
+                    $item['images'][$this->persistenceManager->getIdentifierByObject($node->getProperty($propertyName))] = $node->getProperty($propertyName);
+                }
+            }
+        }
+
+        foreach ($node->getChildNodes('Neos.Neos:ContentCollection,Neos.Neos:Content') as $childNode) {
+            $this->resolveImages($childNode, $item);
+        }
+    }
+
+    /**
+     * Return TRUE/FALSE if the node is currently hidden; taking the "renderHiddenInIndex" configuration
+     * of the Menu Fusion object into account.
+     *
+     * @param NodeInterface $node
+     * @return bool
+     * @throws NodeException
+     */
+    protected function isDocumentNodeToBeIndexed(NodeInterface $node): bool
+    {
+        return !$node->getNodeType()->isOfType('Neos.Neos:Shortcut') && $node->isVisible()
+            && ($this->getRenderHiddenInIndex() || !$node->isHiddenInIndex()) && $node->isAccessible()
+            && $node->getProperty('metaRobotsNoindex') !== true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# neos-seo
+
+A package to enable additional SEO features for Neos CMS. 
+
+[Read the documentation](https://neos-seo.readthedocs.io/en/stable/)
